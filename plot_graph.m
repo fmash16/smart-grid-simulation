ce247d8f79a68a2ffd3e88171d6152ccaecd0a50
fmash16 <fmash16@gmail.com>
2022-05-07 00:13:40 +0600

    huge change to graph plot

--- plot_graph.m
+++ plot_graph.m
@@ -4,47 +4,66 @@ opts.saveFolder = '/home/void/thesis/charts/';
 %opts.saveFolder = '/home/void/thesis/charts/';
 opts.width      = 8;
 opts.height     = 6;
-opts.fontType   = 'Times';
+opts.fontType   = 'Times New Roman';
 opts.fontSize   = 10;
+destFile        = "/home/void/thesis/charts/load28__voltage_current.png"
 
 % create new figure
 fig = figure; clf
 
 % Plot figure
-ftoread = '/home/void/thesis/loads/load_all.csv'
-%ftoread = '/home//thesis/smart-grid-simulation/final/output/LOAD11_data.csv'
+%ftoread = '/home/void/thesis/loads/load_all.csv'
+ftoread = '/home/void/thesis/smart-grid-simulation/final/output/LOAD28_data.csv'
+
+
 fid=fopen(ftoread)
-data=textscan(fid, '%s%s%d%d%d','Delimiter',',','HeaderLines',1)
+%data=textscan(fid, '%s%s%d%d%d','Delimiter',',','HeaderLines',1)
+data=textscan(fid, '%s%f%f%f%f%f%f%f%f%f','Delimiter',',','HeaderLines',8)
 fclose(fid)
 
-x = datetime(data{2},'InputFormat','HH:mm');
-%x = datetime(data{1},'InputFormat','yyyy-MM-dd HH:mm:ss XXX','TimeZone','local','Format','HH:mm:ss');
-y = data{4};
+%x = datetime(data{2},'InputFormat','HH:mm');
+x = datetime(data{1},'InputFormat','yyyy-MM-dd HH:mm:ss XXX','TimeZone','GMT','Format','HH:mm:ss');
+y1 = data{7};
+y2 = data{8};
+y=sqrt(y1.^2+y2.^2)
+%plot(x,y0,'LineWidth',2)
+%hold on
+%plot(x,y,'LineWidth',2)
+%hold on
+yyaxis left
 plot(x,y,'LineWidth',2)
 
-% add axis labes and legend
 % axis tight
 xlabel('time of day (24h)')
-ylabel('load (W)')
-legend('Low Income load','Location','north')
+ylabel('Voltage (V)')
+%legend('Demand','Solar Power Output','Battery Output','north')
 
-% scaling
-% fig.Units               = 'centimeters'
-% fig.Position(3)         = opts.width;
-% fig.Position(4)         = opts.height;
+y1 = data{9};
+y2 = data{10};
+y=sqrt(y1.^2+y2.^2)
+yyaxis right
+ylabel('Current (A)')
+plot(x,y,'LineWidth',2)
+legend('Voltage','Current','north')
+axis on
+grid on
 
 % set text properties
 set(fig.Children, ...
-    'FontName',     'Times', ...
-    'FontSize',     15);
+    'FontName',     'Times New Roman', ...
+    'FontSize',     21);
 
 % remove unnecessary white space
-datetick('x','HH:MM')
-grid on
+t1 = datetime(2019,04,21,0,0,0,'TimeZone','GMT');
+t2 = datetime(2019,04,22,0,0,0,'TimeZone','GMT');
+tick_locations = t1:hours(1):t2;
+set(gca,'XTick',tick_locations);
+datetick('x','HH:mm','keeplimits', 'keepticks')
+set(gca,'XMinorTick','on','YMinorTick','on')
 set(gca,'LooseInset',max(get(gca,'TightInset'), 0.02))
 f=gca
 % export to png
 fig.PaperPositionMode   = 'auto';
 %print([opts.saveFolder 'load_lower'], '-dpng', '-r600');
 
-exportgraphics(f,'/home/void/thesis/charts/load_lower.png','Resolution',600)
+exportgraphics(f,destFile,'Resolution',1000);
